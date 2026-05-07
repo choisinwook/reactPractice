dc5cffb38b0db02244d422ce659b6ce2e1c69a28
sinwookchoi <csinwook@gmail.com>
2026-05-07 09:33:40 +0900

    팝업창 뜨는 버튼 생성 - 20260507

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 function App() {
+    const handleClick = () => {
+        alert('팝업창이 떴어요!'); // Popup appeared!
+    };
+
     return (
         <div>
             <h1>Welcome to My React App</h1>
             <p>This is the main application component.</p>
+            <button onClick={handleClick}>클릭하세요</button>
         </div>
     );
 }
